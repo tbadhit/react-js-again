6c523c1c8b1d78eae6f16665b471b6531a4b056c
Tubagus Adhitya Permana <aditt7116@gmail.com>
2021-10-27 22:26:55 +0700

    first commit

---
+++ ReactPureHtml/like_button.js
@@ -0,0 +1,8 @@
+const e = React.createElement;
+
+function LikeButton() {
+  return e("button", { onClick: () => alert("button work") }, "Like");
+}
+
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
